7be3199d804c0d11929e094406d7ca3f4393b809
setoilareeya <52621278+setoilareeya@users.noreply.github.com>
2022-07-22 17:28:36 +0700

    docker

---
+++ index.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')
+const nodeEnv = process.env.NODE_ENV || 'development' 
+const app = fastify({
+    logger:true
+})
+
+app.get('/', async() => {
+    return{
+        status: 'OK',
+        nodeEnv
+    }
+})
+
+app.listen(3000, '0.0.0.0')
